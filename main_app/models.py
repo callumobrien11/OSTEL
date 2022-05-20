11364048c8bd0c5712c12bf564e9cb2711013279
Just In <jkhclimited@gmail.com>
2022-05-20 12:11:12 -0400

    Removed Image rows from all Models

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django.urls import reverse
 from django.forms import IntegerField
-from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 
 class Hostel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    images = ArrayField(models.CharField(max_length=100, blank=True))
     city = models.CharField(max_length=100) # The longest city name in the world is 85 letters at the moment
 
     def __str__(self):
@@ -27,7 +25,6 @@ class Input(models.Model):
     city = models.CharField(max_length=25)
     description = models.TextField(max_length=1000)
     rating = models.IntegerField()
-    image = models.CharField(max_length=100)
     hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE, related_name='hostel')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
 
