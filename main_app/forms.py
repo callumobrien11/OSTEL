0a8ad047d2622789c3f0b7abdb500e0fedff6f58
Just In <jkhclimited@gmail.com>
2022-05-25 13:36:22 -0400

    Fixed inputs forms and You signed in you sexy thang instead of YOu signed in you sexy thang

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,29 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Input
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class InputForm(ModelForm):
+TYPES = (
+    ('Tip'),
+    ('Recommendation'),
+    ('Review')
+)
+
+class InputForm(forms.ModelForm):
+    title = forms.CharField(max_length=50)
+    type = forms.Select(choices=TYPES)
+    city = forms.CharField(max_length=25)
+    description = forms.CharField(max_length=1000)
+    rating = forms.IntegerField(
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+
     class Meta:
         model = Input
-        fields = ['hostel_name', 'title', 'type', 'description', 'city', 'rating']
+        exclude = ("user", )
         
--- main_app/views.py
+++ main_app/views.py
@@ -48,14 +48,28 @@ def input_list(request):
     inputs = Input.objects.filter(user=request.user)
     return render(request, 'main_app/input_list.html', { 'inputs': inputs })
 
+# @login_required
+# def input_create(request):
+#     input_form = InputForm()
+#     input_form.user = request.user
+#     input_form.save()
+#     return render(request, 'main_app/input_form.html', {
+#         'input_form': input_form
+#     })
+
 @login_required
 def input_create(request):
+    if request.method == "POST":
+        input_form = InputForm(request.POST)
+        if input_form.is_valid():
+            input = input_form.save(commit=False)
+            input.user = request.user
+            input.save()
+            return redirect('input_create')
     input_form = InputForm()
-    return render(request, 'main_app/input_form.html', {
-        'input_form': input_form
-    })
+    return render(request, 'main_app/input_form.html', { 'input_form': input_form })
 
-class InputDetail(DetailView):
+class InputDetail(LoginRequiredMixin, DetailView):
     model = Input
 
 class InputUpdate(LoginRequiredMixin, UpdateView):
